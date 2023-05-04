337d9c888a4790c70bc9b8df3fe392b72f9b44e0
chelseymachin <chelseymachin@gmail.com>
2023-05-04 12:58:15 -0700

    add function in chatbox to get user from userId in props; add template structure for chatbox and placeholder for chatbubble

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { toSvg } from 'html-to-image';
 import { Message, User } from '~~/types';
 
 
@@ -8,8 +9,58 @@ const props = defineProps<{
   me: User
   usersTyping?: User[]
 }>()
+
+function getUser(userId: string) {
+  return props.users.find(user => user.id === userId)
+}
+
+const isOpen = ref(false)
+
+
 </script>
 
 <template>
-  <!-- Code out the chatbox for Exercise 1 here -->
+  <div class="fixed bottom-[10px] right-[10px]">
+    <button 
+      v-show="!isOpen" 
+      class="bg-purple-300 p-3 rounded" 
+      @click="isOpen = true"
+    >
+      <IconChat class="w-8 h-8 text-white" />
+    </button>
+
+    <div 
+      v-if="isOpen" 
+      class="box bg-purple-300 dark:bg-purple-700 w-[400px] rounded"
+    >
+      <header class="bg-purple-200 dark:bg-purple-600 px-4 flex justify-between items-center">
+        Support Chat
+        <button 
+          class="p-4 pr-0" 
+          @click="isOpen = false"
+        >
+          X
+        </button>
+      </header>
+
+
+      <div class="messages p-4 overflow-y-scroll max-h-[80vh]">
+        <ChatBubble
+          v-for="message in messages"
+          :key="message.id"
+          :user="getUser(message.userId)"
+          :message="message"
+        />
+      </div>
+
+
+      <footer class="p-2">
+        <input 
+          type="text" 
+          class="input w-full block" 
+          placeholder="Type your support request here"
+        />
+      </footer>
+    </div>
+  </div>
 </template>
